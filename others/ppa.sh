6852fd5546ca7a76e6c58bb1145ed2e1f66be4b5
zero <boy2000_007man@163.com>
2013-07-23 11:09:21 +0800

    Update ppa.sh

--- others/ppa.sh
+++ others/ppa.sh
@@ -8,3 +8,13 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo add-apt-repository ppa:chris-lea/node.js
 # update your software information
 sudo apt-get update
+# update your pciids, this may take minutes, depend on lan speed
+sudo update-pciids
+# install bumblebee
+sudo apt-get install bumblebee bumblebee-nvidia
+# remove default openjdk
+sudo apt-get remove openjdk-7-jre openjdk-7-jre-headless openjdk-7-jre-lib
+# install oracle jave
+sudo apt-get install oracle-java8-installer oracle-java8-set-default
+# install node.js
+sudo apt-get install nodejs
