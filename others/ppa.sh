380fdba778dde6817441c8a2b5ef9a508ffad147
zero <boy2000_007man@163.com>
2013-08-03 16:38:32 +0800

    modified ppa.sh to make it more clear

--- others/ppa.sh
+++ others/ppa.sh
@@ -1,20 +1,14 @@
-#!/bin/sh
+#!/bin/bash -e
 # This script adds ppa-repository which you may use
-# bumblebee offers Nvidia Optimus tech, save energy and low temp
-sudo add-apt-repository ppa:bumblebee/stable
-# oracle-java offers more stable functions
-sudo add-apt-repository ppa:webupd8team/java
-# node.js offers Server-Side Javascript
-sudo add-apt-repository ppa:chris-lea/node.js
-# update your software information
-sudo apt-get update
-# update your pciids, this may take minutes, depend on lan speed
-sudo update-pciids
-# install bumblebee
-sudo apt-get install bumblebee bumblebee-nvidia
-# remove default openjdk
-sudo apt-get remove openjdk-7-jre openjdk-7-jre-headless openjdk-7-jre-lib
-# install oracle jave
-sudo apt-get install oracle-java8-installer oracle-java8-set-default
-# install node.js
-sudo apt-get install nodejs
+sudo add-apt-repository ppa:bumblebee/stable    # bumblebee offers Nvidia Optimus tech, save energy and low temp
+sudo add-apt-repository ppa:webupd8team/java    # oracle-java offers more stable functions
+sudo add-apt-repository ppa:chris-lea/node.js   # node.js offers Server-Side Javascript
+sudo apt-get update                             # update your software information
+sudo update-pciids                              # update your pciids, this may take minutes, depend on lan speed
+sudo apt-get install bumblebee bumblebee-nvidia # install bumblebee
+sudo apt-get remove openjdk-7-jre \
+    openjdk-7-jre-headless openjdk-7-jre-lib    # remove default openjdk
+sudo apt-get install oracle-java8-installer \
+    oracle-java8-set-default                    # install oracle jave
+sudo apt-get install nodejs                     # install node.js
+exit 0
