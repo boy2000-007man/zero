e131f8ada192d73dd387af8e4d83d4e02f7c8ee1
zero <boy2000_007man@163.com>
2013-08-14 17:16:32 +0800

    version 0.0.3

---
+++ ant/main.cpp
@@ -0,0 +1,7 @@
+#include "sdk.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
--- ant/sdk.h
+++ ant/sdk.h
@@ -23,7 +23,12 @@ struct Location {
     friend bool operator!= (const Location &l1, const Location &l2) {
         return !(l1 == l2);
     }
-} home(0, 0);   // struct Location
+};  // struct Location
+struct Home : public Location {
+    int food;
+    Home(const int &tx, const int &ty) : x(tx), y(ty), food(0) {
+    }
+}
 int distance(const Location &l1, const Location &l2) {
     return abs(l1.x - l2.x) + abs(l1.y - l2.y);
 }
@@ -31,10 +36,38 @@ bool cmp(const Location &l1, const Location &l2) {
     return l1 < l2;
 }
 struct Food : public Location {
-    int number;
+    int food;
+    Food(const int &tx, const int &ty, const int &vol) : x(tx), y(ty), volume(vol) {
+    }
 };  // struct Food
 struct Ant : public Location {
+    int food;
     std::vector<Food> foods;
+    Ant(const int &tx, const int &ty) : x(tx), y(ty), food(0) {
+    }
+    void moveto(const Location &target) {
+        if (x < target.x)
+            x++;
+        if (x > target.x)
+            x--;
+        if (y < target.y)
+            y++;
+        if (y > target.y)
+            y--;
+    }
+    void action() {
+        if (food != 0) {
+            moveto(Map::home);
+        } else if (foods.empty()) {
+            moveto(Location(rand() % Map::x, rand() % Map::y));
+        } else {
+            Location nearfood = foods[0];
+            for (int i = 0; i < foods.size(); i++)
+                if (distance(*this, nearfood) > distance(*this, foods[i]))
+                    nearfood = foods[i];
+            moveto(nearfood);
+        }
+    }
 };  // struct Ant
 static int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1},
            dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
@@ -53,45 +86,57 @@ void merge(std::vector<T> &target, const std::vector<T> &source) {
             j++;
         }
     }
+    while (i < target.size())
+        temp.push_back(target[i++]);
+    while (j < source.size())
+        temp.push_back(source[j++]);
+    target = temp;
 }
 class Map {
-    int x_max, y_max;
+    static int x, y;
+    static Home home;
     std::vector<Ant> ants;
-    void action(const int &num) {
-        Ant &ant = ants[num];
-        if (!ant.foods.empty()) {
-            Location nearfood = ant.foods[0];
-            for (int i = 0; i < ant.foods.size(); i++)
-                if (distance(ant, nearfood) > distance(ant, ant.foods[i]))
-                    nearfood = ant.foods[i];
-            if (ant.x < nearfood.x)
-                ant.x++;
-            else if (ant.x > nearfood.x)
-                ant.x--;
-            if (ant.y < nearfood.y)
-                ant.y++;
-            else if (ant.y > nearfood.y)
-                ant.y--;
-            return ;
-        }
-        const int t = rand() % num;
-        ant.x = (ant.x + dx[t]) % x_max;
-        ant.y = (ant.y + dy[t]) % y_max;
-    }
+    std::vector<Food> foods;
 public:
-    Map(const int &x, const int &y) : x_max(x), y_max(y) {
+    Map(const int &x, const int &y) : map_x(x), map_y(y) {
     }
     void flash() {
         sort(ants.begin(), ants.end(), cmp);
-        for (int i = 0; i < ants.size(); i++) {
+        for (int i = 0; i < ants.size(); ) {
             int same = 1;
             while (i + same < ants.size() && ants[i] == ants[i + same])
                 same++;
-            std::vector<Food> foods;
-            for (int j = 0; j < same; j++) {
+            for (int j = 1; j < same; j++) {
+                merge(ant[i].foods, ant[i + j].foods);
             }
+            for (int j = 1; j < same; j++) {
+                ant[i + j].foods = ant[i].foods;
+            }
+            i += same;
+        }
+        int food[map_x][map_y];
+        for (int i = 0; i < map_x; i++)
+            for (int j = 0; j < map_y; j++)
+                food[i][j] = 0;
+        for (int i = 0; i < foods.size(); i++)
+            food[foods.x][foods.y] = foods.food;
+        for (int i = 0; i < ants.size(); i++) {
+            Ant &ant = ants[i];
+            if (food[ant.x][ant.y] != 0) {
+                merge(ant.foods, std::vector<Food>(1, Food(ant.x, ant.y, food[ant.x][ant.y])));
+                if (ant.food == 0) {
+                    ant.food++;
+                }
+            }
+            if (ant == Map::home) {
+                Map::home.food += ant.food;
+                ant.food = 0;
+            }
+            ant.action();
         }
     }
-}
+};  // class Map
+int Map::x(100), Map::y(100);
+Home Map::home(0, 0);
 }   // namespace sdk
 #endif  // __SDK_H__
